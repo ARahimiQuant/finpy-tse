b25ed6462bfdf5fc98400382d2ead4c33fb92cfd
mtrshirazi21 <mtrshirazi21@gmail.com>
2022-08-19 01:03:35 +0430

    change license

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     name='finpy_tse',                           # should match the package folder
     packages=['finpy_tse'],                     # should match the package folder
     version='1.1.0',                                # important for updates
-    license='GPL-3.0',                                  # should match your chosen license
+    license='BSD (3-clause)',                                  # should match your chosen license
     description='A Python Module to Access Tehran Stock Exchange Historical and Real-Time Data',
     long_description=long_description,              # loads your README.md
     long_description_content_type="text/markdown",  # README.md is of type 'markdown'
@@ -18,4 +18,3 @@ setuptools.setup(
     install_requires=['requests','jdatetime','pandas','numpy','requests','bs4','asyncio','urllib3','aiohttp','unsync','IPython','persiantools','datetime','XlsxWriter','lxml'],                  # list all packages that your package uses
     
 )
-
